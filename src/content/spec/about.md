cef2b8cff650e10b8b94ecc755170b4890f364a1
yeuxdhiver <lamngan1705@gmail.com>
2025-07-16 18:33:34 +0700

    meow

--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,7 +1,7 @@
 # About
-This is the demo site for [Fuwari](https://github.com/saicaca/fuwari).
+This is the demo site for [Cherrybie](https://github.com/kawaiicassie/cherrybie).
 
-::github{repo="saicaca/fuwari"}
+::github{repo="aicaca/fuwari"}
 
 > ### Sources of images used in this site
 > - [Unsplash](https://unsplash.com/)
---
+++ src/i18n/languages/vi.ts
@@ -0,0 +1,38 @@
+import Key from "../i18nKey";
+import type { Translation } from "../translation";
+
+export const en: Translation = {
+    [Key.home]: "Trang chủ",
+    [Key.about]: "Về tôi",
+    [Key.archive]: "Lưu trữ",
+    [Key.search]: "Tìm kiếm",
+
+    [Key.tags]: "Thẻ",
+    [Key.categories]: "Danh mục",
+    [Key.recentPosts]: "Bài viết gần đây",
+
+    [Key.comments]: "Bình luận",
+
+    [Key.untitled]: "Không có tiêu đề",
+    [Key.uncategorized]: "Không có danh mục",
+    [Key.noTags]: "Không có thẻ",
+
+    [Key.wordCount]: "từ",
+    [Key.wordsCount]: "từ",
+    [Key.minuteCount]: "phút",
+    [Key.minutesCount]: "phút",
+    [Key.postCount]: "bài viết",
+    [Key.postsCount]: "bài viết",
+
+    [Key.themeColor]: "Chế độ chủ đề",
+
+    [Key.lightMode]: "Sáng",
+    [Key.darkMode]: "Tối",
+    [Key.systemMode]: "Hệ thống",
+
+    [Key.more]: "Xem thêm",
+
+    [Key.author]: "Tác giả",
+    [Key.publishedAt]: "Đăng vào",
+    [Key.license]: "Giấy phép",
+};
